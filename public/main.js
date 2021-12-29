95fbee1065a44f5e1812ec7a4b92f98afdd51788
sbaisden25 <spbaisden@gmail.com>
2021-12-29 16:32:51 -0500

    Fig bug when keys held down

--- public/main.js
+++ public/main.js
@@ -1,11 +1,31 @@
+
+let justPlayed = null;
+
 function removeTransition(e) {
     if (e.propertyName !== 'transform') return;
     e.target.classList.remove('playing');
 }
 
 function playSound(e) {
+
+    if (e.keyCode === justPlayed) return; // If same key has just been played without key being released, don't play again
+
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
     const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
+
+
+    if (!audio) return;
+
+    key.classList.toggle('playing');
+    audio.currentTime = 0;
+    audio.play();
+    justPlayed = e.keyCode; // Set the key that has just been played
+}
+
+function playSoundOnClick(e) {
+    const audio = document.querySelector(`audio[data-key="${e.target.dataset.key}"]`);
+    const key = document.querySelector(`div[data-key="${e.target.dataset.key}"]`);
+
     if (!audio) return;
 
     key.classList.add('playing');
@@ -16,45 +36,18 @@ function playSound(e) {
 
 const keys = Array.from(document.querySelectorAll('.key'));
 keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+keys.forEach(key => key.addEventListener('click', playSoundOnClick));
+
 window.addEventListener('keydown', playSound);
+window.addEventListener('keyup', () => justPlayed = null); // Allow key to be played again
 
-/*
 
-class Keys {
-    constructor(title, keyCode, audio, row) {
-        this.title = title;
-        this.keyCode = keyCode;
-        this.audio = audio;
-        this.row = row;
-    }
+
+
+function stopAllSounds(e) {
+    const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
+    const allAudio = document.querySelectorAll('audio');
+    allAudio.forEach(audio => audio.pause());
 }
 
-var famousKeys = [{
-    title: "Intro",
-    keyCode: "81",
-    audio: "sounds/famous/manintro.mp3"
-}, {
-    title: "Beat",
-    data_key: "87",
-    src: "sounds/famous/sample.wav.mp3"
-}];
 
-// Write class that will create keys
-class Key {
-    constructor(title, keyCode, audio) {
-        this.title = title;
-        this.keyCode = keyCode;
-        this.audio = audio;
-    }
-    render() {
-        const key = document.createElement('div');
-        key.setAttribute('data-key', this.keyCode);
-        key.classList.add('key');
-        key.innerHTML = `
-        <kbd>${this.title}</kbd>
-        <span class="sound">${this.title}</span>
-        `;
-        return key;
-    }
-}
-*/
